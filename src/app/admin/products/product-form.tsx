33bf8265cd62ca9734689ed3b08a1752c837a4a2
Satya Ravi S <svsravichandra@gmail.com>
2025-06-25 15:56:58 +0000

    done

--- src/app/admin/products/product-form.tsx
+++ src/app/admin/products/product-form.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useForm, SubmitHandler } from 'react-hook-form';
+import { useForm, SubmitHandler, Controller } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 import { Button } from '@/components/ui/button';
