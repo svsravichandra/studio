22a4d9fef8fae28e6ca73156ac00c322cc421cb7
Satya Ravi S <svsravichandra@gmail.com>
2025-06-26 04:25:19 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,6 @@ const {fontFamily} = require('tailwindcss/defaultTheme');
 
 export default {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
